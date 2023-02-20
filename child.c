3832e9f108f987c66ef672ae74516528b7d12795
BromicOuma <oumabromic7@gmail.com>
2023-02-20 01:20:02 -0800

    ::

---
+++ child.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * child - function for child process created
+ * @fullpath: full path of the executable
+ * @tokens: tokenized user input
+ * Return: returm 0 on success and -1 on error
+ */
+int child(char *fullpath, char **tokens)
+{
+	pid_t child_pid;
+	int status;
+	int execve_status;
+	char **envp = environ;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		errors(1);
+		exit(EXIT_FAILURE);
+	}
+	if (child_pid == 0)
+	{
+		execve_status = execve(fullpath, tokens, envp);
+		if (execve_status == -1)
+			return (-1);
+	}
+	else
+		wait(&status);
+
+	return (0);
+}
